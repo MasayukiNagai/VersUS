024cd3ad412de59bc01e01066c758d3f2e50f1f1
Moon <masayukinagai_2022@depauw.edu>
2019-10-01 23:03:16 -0400

    added getNP_protein function

--- MissenseMutations/__main__.py
+++ MissenseMutations/__main__.py
@@ -10,44 +10,63 @@ readHumanEnzymes = Entrez.read(handleHumanEnzymes)
 print("{} enzymes are found".format(readHumanEnzymes["Count"]))
 
 
-def getGenes(xmlfile):
-    tree = ET.parse(xmlfile)
-    root = tree.getroot()
-    qualifierNames = dict()
-    for child in root.iter('GBQualifier'):
-        name = child.find('GBQualifier_name').text
-        qualifierNames[name] = True
-    if 'gene' in qualifierNames:
-        return True
-    else:
-        return False
-
-
-def getHumanEnzymes(idLists):
+def getNP_protein(idLists):
     human_enzymes = set()
 
     for i in idLists:
         protein_info = Entrez.efetch(db='protein', id=i, retmode='xml')
-        gene = getGenes(protein_info)
-        print(gene)  # delete later
-        if gene:
-            human_enzymes.add(i)
-        protein_info.close()
-        print(i)  # delete later
+        tree = ET.parse(protein_info)
+        root = tree.getroot()
+        np = root.find('./GBSeq/GBSeq_accession-version')
+        human_enzymes.add(np.text)
 
     return human_enzymes
+
+
+# np_human_enzymes = getNP_protein(readHumanEnzymes['IdList'][0:10])
+# print(np_human_enzymes)
+
+# def getGenes(xmlfile):
+#     tree = ET.parse(xmlfile)
+#     root = tree.getroot()
+#     qualifierNames = dict()
+#     for child in root.iter('GBQualifier'):
+#         name = child.find('GBQualifier_name').text
+#         qualifierNames[name] = True
+#     if 'gene' in qualifierNames:
+#         return True
+#     else:
+#         return False
+#
+#
+# def getHumanEnzymes(idLists):
+#     human_enzymes = set()
+#
+#     for i in idLists:
+#         protein_info = Entrez.efetch(db='protein', id=i, retmode='xml')
+#         gene = getGenes(protein_info)
+#         print(gene)  # delete later
+#         if gene:
+#             human_enzymes.add(i)
+#         protein_info.close()
+#         print(i)  # delete later
+#
+#     return human_enzymes
 
 
 # human_enzymes = getHumanEnzymes(readHumanEnzymes['IdList'][100:110])
 # print(human_enzymes)
 
-handleMissense = Entrez.esearch(db='clinvar', retmax=20000,
+handleMissense = Entrez.esearch(db='clinvar',
                                 term='( ( ("clinsig has conflicts"[Properties]) OR ("clinsig vus"[Properties]) ) AND ("missense variant"[molecular consequence] OR "SO 0001583"[molecular consequence]))')
 recordsMissense = Entrez.read(handleMissense)
 print("{} genes are found".format(recordsMissense['Count']))
 print(recordsMissense)
-detailMissesnse = Entrez.efetch(db='clinvar', id=558834, rettype='vcv', is_variationid="true", from_esearch="true")
-print(detailMissesnse.read())
+# detailMissesnse = Entrez.efetch(db='clinvar', id=558834, rettype='vcv', is_variationid="true", from_esearch="true")
+# print(detailMissesnse.read())
+
+def getNP_Clinvar(idLists):
+
 
 # nuccore xml MM
 aatranlation = {'Ala': 'A', 'Arg': 'R', 'Asn': 'N', 'Asp': 'D', 'Cys': 'C',
