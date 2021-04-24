442b2a58f6cc0ebe77b72a46d997b9e9dca54000
Moon <masayukinagai_2022@depauw.edu>
2021-04-23 21:35:53 -0400

    Fix a minor error

--- src/Handlers/CADDHandler.py
+++ src/Handlers/CADDHandler.py
@@ -136,8 +136,8 @@ class CADDHandler:
             cadd_dict[chrom][pos]['alt'].append(alt)
         header_info = ('CHROM', 'POS', 'REF', 'ALT')
         header = '#' + '\t'.join(header_info)
-        chroms = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
-                  '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'.
+        chroms = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                  '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
                   '21', '22', 'X')
         with open(self.cadd_input, 'w') as f:
             f.write(header + '\n')
