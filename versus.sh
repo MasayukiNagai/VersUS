ed77e45d19394b9bf59b813659cb5e78551a58f3
Moon <masayukinagai_2022@depauw.edu>
2022-03-02 21:49:29 -0500

    Minor update

--- versus.sh
+++ versus.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
+TIMESTAMP=`date "+%Y%m%d"`
 
-pwd=`pwd`
-mkdir -p $pwd/data/fasta
+PWD=`pwd`
+mkdir -p $PWD/data/fasta
 
 ### Prepare input files
 # Download the ClinVarVariationRelease.xml.gz
-cd $pwd/data
+cd $PWD/data
 wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/clinvar_variation/ClinVarVariationRelease_00-latest.xml.gz
 
 # Download the fasta sequences
-cd $pwd/data/fasta
+cd $PWD/data/fasta
 for i in {1..100}
 do
    wget https://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/human.$i.protein.faa.gz
 done
 
 # Download the tsv file of a list of human enzymes from Uniprot
-# cd $pwd/data
+# cd $PWD/data
 # python getHumanEnzymesFromUniprot.py
 
 ### Execute the VersUS
-
+python VersUS.py -i $PWD/config.conf -n $TIMESTAMP
 
 ### Register the output to the MySQL database
 # pythoh versusDBsql.py ...
