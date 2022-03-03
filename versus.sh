197b24d80bf7af1ec29cdd7d5722323a4f89b842
Moon <masayukinagai_2022@depauw.edu>
2022-03-02 21:44:19 -0500

    Add shell script

---
+++ versus.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+pwd=`pwd`
+mkdir -p $pwd/data/fasta
+
+### Prepare input files
+# Download the ClinVarVariationRelease.xml.gz
+cd $pwd/data
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/clinvar_variation/ClinVarVariationRelease_00-latest.xml.gz
+
+# Download the fasta sequences
+cd $pwd/data/fasta
+for i in {1..100}
+do
+   wget https://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/human.$i.protein.faa.gz
+done
+
+# Download the tsv file of a list of human enzymes from Uniprot
+# cd $pwd/data
+# python getHumanEnzymesFromUniprot.py
+
+### Execute the VersUS
+
+
+### Register the output to the MySQL database
+# pythoh versusDBsql.py ...
