b681557161765944463ebcd00afeb94991c13820
Moon <masayukinagai_2022@depauw.edu>
2022-05-16 23:50:08 -0400

    Add logger

--- src/statistics.py
+++ src/statistics.py
@@ -1,5 +1,5 @@
 import argparse
-from datetime import datetime
+import logging
 from Handlers.SeqHandler import *
 from Handlers.ClinVarHandler import *
 
@@ -14,10 +14,30 @@ def argument_parser():
     return args
 
 
+def setup_logger(name: str, logfile: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    # creates a file handler that logs messages above DEBUG level
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(logging.DEBUG)
+    fh_formatter = logging.Formatter('[%(asctime)s] %(levelname)s %(filename)s %(funcName)s : %(message)s')
+    fh.setFormatter(fh_formatter)
+    # creates a file handler that logs messages above INFO level
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.DEBUG)
+    sh_formatter = logging.Formatter('[%(asctime)s] %(levelname)s %(filename)s : %(message)s', '%Y-%m-%d %H:%M:%S')
+    sh.setFormatter(sh_formatter)
+    # add the handlers to logger
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+    return logger
+
+
 def main():
     args = argument_parser()
     genelist = args.gene
     xml = args.xml
+    logger = setup_logger('versus_logger', 'vus_stats.log')
     seqHandler = SeqHandler(genelist, None)
     genes_dict = seqHandler.readUniprot_GeneId_EC()
     clinvarHandler = ClinVarHandler(xml)
